29e20c98dfcbc1c91e255cf94bf2c08a1c4bd670
Melvin Beverwijk <d.m.beverwijk@gmail.com>
2020-02-22 03:07:42 +0100

    setup user controller and added register action

---
+++ controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace app\controllers;
+
+use app\models\LoginForm;
+use app\models\RegisterForm;
+use Yii;
+use yii\web\Controller;
+use yii\web\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Register action.
+     *
+     * @return Response|string
+     */
+    public function actionRegister()
+    {
+        //TODO rebuild this action so it can create a registration form
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new RegisterForm();
+        if ($model->load(Yii::$app->request->post()) && $model->register()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('register', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+}
--- models/User.php
+++ models/User.php
@@ -13,12 +13,15 @@ class User extends ActiveRecord
     public $accessToken;
 
 
+    //TODO add registration function
+
     /**
      * {@inheritdoc}
      */
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        $command = Yii::app()->db->createCommand();
+        return $command->select('*')->from('user')->where('id=:id', array(':id' => $id))->queryAll();
     }
 
     /**
@@ -26,13 +29,8 @@ class User extends ActiveRecord
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        $command = Yii::app()->db->createCommand();
+        return $command->select('*')->from('user')->where('accessToken=:token', array(':token' => $token))->queryAll();
     }
 
     /**
@@ -43,13 +41,8 @@ class User extends ActiveRecord
      */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        $command = Yii::app()->db->createCommand();
+        return $command->select('*')->from('user')->where('username=:username', array(':username' => $username))->queryAll();
     }
 
     /**
