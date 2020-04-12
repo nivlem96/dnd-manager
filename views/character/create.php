93f8d3be8d96407da6073863b6e634b544376c33
Melvin Beverwijk <d.m.beverwijk@gmail.com>
2020-04-12 14:07:29 +0200

    stats are working

--- views/character/create.php
+++ views/character/create.php
@@ -39,6 +39,30 @@ $this->title = 'Create character';
 	</div>
     <?= $form->field($model, 'background')->textarea() ?>
 	<?= $form->field($model,'player_id')->hiddenInput(['value'=>Yii::$app->user->id])->label(false) ?>
+	<div class="form-group">
+		<table style="width: 100%">
+			<tr>
+				<th>Strength</th>
+				<th>Dexterity</th>
+				<th>Constitution</th>
+			</tr>
+			<tr>
+				<td><?= $form->field($model,'strength')->input('number',['min'=>0])->label(false) ?></td>
+				<td><?= $form->field($model,'dexterity')->input('number',['min'=>0])->label(false) ?></td>
+				<td><?= $form->field($model,'constitution')->input('number',['min'=>0])->label(false) ?></td>
+			</tr>
+			<tr>
+				<th>Intelligence</th>
+				<th>Wisdom</th>
+				<th>Charisma</th>
+			</tr>
+			<tr>
+				<td><?= $form->field($model,'intelligence')->input('number',['min'=>0])->label(false) ?></td>
+				<td><?= $form->field($model,'wisdom')->input('number',['min'=>0])->label(false) ?></td>
+				<td><?= $form->field($model,'charisma')->input('number',['min'=>0])->label(false) ?></td>
+			</tr>
+		</table>
+	</div>
 
 	<div class="form-group">
 		<div class="col-lg-offset-1 col-lg-11">
